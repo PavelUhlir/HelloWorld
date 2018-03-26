af3a3d9ce059de4222aca2751941f752bc20e401
pavel.uhlir <pavel.uhlir@W10103.sps-cl.local>
2018-03-26 12:22:08 +0200

    prvni commit

---
+++ src/nazdar/světe/NazdarSvěte.java
@@ -0,0 +1,21 @@
+
+package nazdar.světe;
+
+/**
+ *
+ * @author pavel.uhlir
+ */
+public class NazdarSvěte {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {      
+        System.out.println("Nazdar bazar!");
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i+1 + " ");
+        }
+    }
+    
+}
